fdf3be669ea7048a447d49189a44c0019dd4c222
Annie Liao <liaowow@yahoo.com>
2019-11-11 18:05:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
+  new_arr = []
   counter = 0
   while array[counter] do
-    puts array[counter] ** 2
+    new_arr.push(array[counter] ** 2)
     counter += 1
   end
+  return new_arr
 end
